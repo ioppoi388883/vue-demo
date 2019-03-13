5933d1dcccc629c81ddb47942ac71d53735e4834
ioppoi388883 <ioppoi388883@gmail.com>
2019-03-13 15:29:37 +0800

    vue项目上传github测试

---
+++ vue-demo/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>菜鸟教程</h1>
+    <p>学的不仅是技术，更是梦想！</p>
+    <p>哈哈哈，打字辛苦啊！！！</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Test',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
